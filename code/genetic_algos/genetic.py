e2d1d4a66c4d0e1b8094508a5681daa32c730aec
Nick Jalbert <nickjalbert@gmail.com>
2020-06-13 14:08:35 -0700

    minor cleanup of genetic algo

--- code/genetic_algos/genetic.py
+++ code/genetic_algos/genetic.py
@@ -20,9 +20,21 @@ import mlflow.tracking
 import gym
 
 
+def train():
+    GENERATIONS = 1000
+    child_pool = get_child_pool()
+    fittest = [random.choice(child_pool) for i in range(10)]
+    for generation in range(GENERATIONS):
+        generate_children(fittest, child_pool)
+        results = evaluate_children(child_pool)
+        evaluate_results(results, generation)
+        fittest = [r[1] for r in results]
+
+
 def get_child_pool():
     CHILD_COUNT = 100
     children = []
+    # Allocating a new keras NN takes a while, instead reuse a pool of nets
     for i in range(CHILD_COUNT):
         model = tf.keras.Sequential(
             [
@@ -39,32 +51,15 @@ def get_child_pool():
     return children
 
 
-def get_run_batch(agent):
-    RUNS_PER_BATCH = 10
-    return [get_run(agent, True) for i in range(RUNS_PER_BATCH)]
-
-
-def get_run(agent, explore):
-    env = gym.make("CartPole-v1")
-    state = env.reset()
-    done = False
-    total_reward = 0
-    run = []
-    p_lefts = []
-    while not done and total_reward < 200:
-        p_left = float(agent(np.reshape(state, (1, 4)))[0][0])
-        p_lefts.append(p_left)
-        if explore:
-            action = 0 if random.random() <= p_left else 1
-        else:
-            action = 0 if p_left >= 0.5 else 1
-        state, reward, done, info = env.step(action)
-        total_reward += reward
-        run.append((state, action))
-    if not explore:
-        print(p_lefts)
-        print([a for s, a in run])
-    return run
+def generate_children(fittest, child_pool):
+    # Create new, randomly-perturbed children
+    for child in child_pool:
+        parent = random.choice(fittest)
+        create_child(child, parent, with_noise=True)
+    # Bring the previous fittest nets directly into the new generation
+    for fit in fittest:
+        child = random.choice(child_pool)
+        create_child(child, fit, with_noise=False)
 
 
 def create_child(child, parent, with_noise):
@@ -80,23 +75,36 @@ def create_child(child, parent, with_noise):
     return child
 
 
-def generate_children(fittest, child_pool):
-    for child in child_pool:
-        parent = random.choice(fittest)
-        create_child(child, parent, with_noise=True)
-    for fit in fittest:
-        child = random.choice(child_pool)
-        create_child(child, fit, with_noise=False)
-
-
 def evaluate_children(child_pool):
-    FITTEST = -10
+    FITTEST_CUTOFF = -10
     results = []
     for child in child_pool:
         runs = get_run_batch(child)
         mean_run_length = statistics.mean([len(run) for run in runs])
         results.append((mean_run_length, child))
-    return sorted(results, key=lambda x: x[0])[FITTEST:]
+    return sorted(results, key=lambda x: x[0])[FITTEST_CUTOFF:]
+
+
+def get_run_batch(agent):
+    RUNS_PER_BATCH = 10
+    return [get_run(agent) for i in range(RUNS_PER_BATCH)]
+
+
+def get_run(agent):
+    env = gym.make("CartPole-v1")
+    state = env.reset()
+    done = False
+    total_reward = 0
+    run = []
+    p_lefts = []
+    while not done and total_reward < 200:
+        p_left = float(agent(np.reshape(state, (1, 4)))[0][0])
+        p_lefts.append(p_left)
+        action = 0 if random.random() <= p_left else 1
+        state, reward, done, info = env.step(action)
+        total_reward += reward
+        run.append((state, action))
+    return run
 
 
 def evaluate_results(results, generation):
@@ -117,17 +125,6 @@ def evaluate_results(results, generation):
     print(f"\tMedian steps per run {statistics.median(mean_run_lengths)}")
     print(f"\tMean steps per run {statistics.mean(mean_run_lengths)}")
     print(f"\tMin steps per run {min(mean_run_lengths)}")
-
-
-def train():
-    GENERATIONS = 1000
-    child_pool = get_child_pool()
-    fittest = [random.choice(child_pool) for i in range(10)]
-    for generation in range(GENERATIONS):
-        generate_children(fittest, child_pool)
-        results = evaluate_children(child_pool)
-        evaluate_results(results, generation)
-        fittest = [r[1] for r in results]
 
 
 if __name__ == "__main__":
